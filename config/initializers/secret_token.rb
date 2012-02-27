2955c66b3b3495e9fa3c2330bb76f31dd8a8f847
Sazor <ZorroCT@gmail.com>
2012-02-27 15:25:40 +0400

    Start working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AvtoSpace::Application.config.secret_token = 'b81c8a2aa24aeb40ed1cd9792ae7621aa5a1be6be9d30b4722f5cb0f8c4fe46cd2100ed19add28193a645cfe1d49163b2b67f645b20987ce3a8d204847d1f0a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AvtoSpace::Application.config.session_store :cookie_store, :key => '_avto_space_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AvtoSpace::Application.config.session_store :active_record_store
